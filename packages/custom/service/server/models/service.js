3e2aeeed0d1bf7b3181f8ad648ae0d35a0a65620
papesdiop <papesdiop@gmail.com>
2014-12-04 16:31:11 +0000

    Service model completed

---
+++ packages/custom/service/server/models/service.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+/**
+ * Service Schema
+ */
+var ServiceSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  technology: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  scope: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  averageCost: {
+    type: Number,
+    required: true,
+    trim: true
+  },
+  hourlyRate: {
+    type: Number,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  staff: { //staff Capacity
+    type: Number,
+    required: true
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  company: {
+    type: Schema.ObjectId,
+    ref: 'Company'
+  }
+});
+
+/**
+ * Validations
+ */
+ServiceSchema.path('type').validate(function(type) {
+  return !!type;
+}, 'Type cannot be blank');
+
+/**
+ * Statics
+ */
+ServiceSchema.statics.load = function(id, cb) {
+  this.findOne({
+    _id: id
+  }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('Service', ServiceSchema);
